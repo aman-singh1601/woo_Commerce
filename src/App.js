c362d686b17935f929ab2c8173d374f9357c3e16
Aman <aman16singh2001@gmail.com>
2023-06-19 22:15:49 +0530

    setting up the router structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Checkout from './components/checkout/Checkout';
+import Login from './components/Login/Login';
+
 
 function App() {
   return (
+    <Router>
     <div className="app">
-     
+     <Routes >
+      <Route path='/' element={<Home/>}/>
+      <Route path='/checkout' element={<Checkout/>}/>
+      <Route path='/login' element={<Login/>}/>
+
+     </Routes>
     </div>
+    </Router>
   );
 }
 
