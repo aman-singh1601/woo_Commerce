5cdb45c6dd46f10d0f6f0989bccabbb23dd5bcf2
Aman <aman16singh2001@gmail.com>
2023-06-20 00:03:52 +0530

    done with the header part

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -1,16 +1,54 @@
 import React from 'react'
 import logo_image from '../../../images/logo_image.png'
-
+import {Link} from 'react-router-dom'
 import './Header.css'
+
+//icons
+import { Search,ShoppingBasket} from '@mui/icons-material'
+
+
 function Header() {
   return (
    <nav className='header'>
+    <Link to='/login'>
         <img
         className='header_logo'
         src={logo_image}
         alt =''
-
         />
+    </Link>   
+    <div className='header_search'>
+    <input type='text' className='header_searchInput'/>
+    <Search className='header_searchIcon'/>    
+    </div> 
+    <div className='header_nav'>
+        <Link to='/login' className='header_link'>
+          <div className='header_option'>
+           <span className='header_optionLine1'>Hello</span> 
+           <span className='header_optionLine2'>Sign In</span> 
+          </div>   
+        </Link>
+        <Link to='/' className='header_link'>
+          <div className='header_option'>
+           <span className='header_optionLine1'>InReturns</span> 
+           <span className='header_optionLine2'>& Orders</span> 
+          </div>   
+        </Link>
+        <Link to='/' className='header_link'>
+          <div className='header_option'>
+           <span className='header_optionLine1'>Your</span> 
+           <span className='header_optionLine2'>Prime</span> 
+          </div>   
+        </Link>
+        <Link to='/checkout' className='header_link'>
+            <div className='header_optionBasket'>
+                <ShoppingBasket/>
+                <span>0</span>
+            </div>
+        </Link>
+        
+    </div>
+   
    </nav>
   )
 }
