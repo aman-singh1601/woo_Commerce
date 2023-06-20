693f40c4087ddfe304c9ee86c3335db687a356b0
Aman <aman16singh2001@gmail.com>
2023-06-20 18:18:34 +0530

    built the banner

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import './Home.css'
 import Header from './Header/Header'
+
+import banner from '../../images/bannerv2.jpg'
+
 function Home() {
   return (
     <div>
         <Header/>
+        <div className='home'>
+            <img
+              className='home_image'
+              src={banner}
+              alt=''
+            />
+        </div>
+
     </div>
   )
 }
