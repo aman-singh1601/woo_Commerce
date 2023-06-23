1a3f883574d7d5d4f9377f7c5c3f857a6dbb4433
Aman <aman16singh2001@gmail.com>
2023-06-23 13:42:10 +0530

    building the checkout page

--- src/components/checkout/Checkout.js
+++ src/components/checkout/Checkout.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import './Checkout.css'
 import Header from '../Home/Header/Header'
+import { useStateValue } from '../StateProvider/StateProvider'
 
 
 function Checkout() {
+    const [{basket},dispatch]=useStateValue()
   return (
     <div>
       <Header/>
-      Checkout
+      <div className='checkout'>
+        <img 
+        className='checkout_ad'
+        src='https://images.pexels.com/photos/949587/pexels-photo-949587.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+        alt=''
+        />
+      </div>
     </div>
   )
 }
