4456f0269847a3e4491278a990247a8d4ba96126
Aman <aman16singh2001@gmail.com>
2023-06-24 19:55:36 +0530

    built the login structure

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,42 @@
 import React from 'react'
 import './Login.css'
+import { Link } from 'react-router-dom'
+import  logoImage from'../../images/logo_image.png'
 
 
 function Login() {
+  const login= event=>{
+    event.preventDefault()
+  }
   return (
-    <div>Login</div>
+    <div className='login'>
+      <Link to='/'>
+        <img
+        className='login_logo'
+        src={logoImage}
+        alt=''
+        />
+      </Link>
+      <div className='login_container'>
+        <h1>Sign In</h1>
+        <form className='login_form'>
+          <h5>E-mail</h5>
+          <input type='email'/>
+          <h5>Password</h5>
+          <input type='password'/>
+         
+         <button
+         onClick={login}
+         type='submit'
+         className='login_signInButton'><span>sign in</span></button>
+          <button
+          onClick={register}
+          className='login_createAccButton'><span>Create New Account</span></button>
+        
+        </form>
+
+      </div>
+    </div>
   )
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp ({
+  apiKey: "AIzaSyBI9jl-zrXT-yej3b1cM-RoT-tzESggnqs",
+  authDomain: "woo-commerce-bd194.firebaseapp.com",
+  projectId: "woo-commerce-bd194",
+  storageBucket: "woo-commerce-bd194.appspot.com",
+  messagingSenderId: "220304581261",
+  appId: "1:220304581261:web:8d66c136a20620269bf8c4"
+});
+
+const db=firebaseApp.firestore();
+const auth=firebase.auth();
+
+export {db,auth}
