d7346c70fcc77fb61d51abdf3085f6b8e5ae3362
Aman <aman16singh2001@gmail.com>
2023-06-27 12:14:24 +0530

    implemented stripe and added currencyformat in payment page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ import { useEffect } from 'react';
 import { auth } from './firebase';
 import Payment from './components/Payment/Payment';
 
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+const promise =loadStripe(
+  'pk_test_51NNBNSSC9WSgNAb5RGoc4PTgY0NcnlnJMUI0xHggkgsIMlRsALW3CvOy7ciSsjIlHKaPVjwvSJiNvnb9Kb8U6ZDq00FGk8iyqN'
+)
 
 function App() {
   const [{user},dispatch]=useStateValue()
@@ -38,7 +44,13 @@ function App() {
       <Route path='/' element={<Home/>}/>
       <Route path='/checkout' element={<Checkout/>}/>
       <Route path='/login' element={<Login/>}/>
-      <Route path='/payment' element={<Payment/>}/>
+      <Route path='/payment' element=
+      { <Elements stripe={promise}>
+      <Payment/>
+      </Elements>
+      } >
+     
+      </Route>
 
      </Routes>
     </div>
--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../Home/Header/Header'
 import './Payment.css'
 import { useStateValue } from '../StateProvider/StateProvider'
 import CheckoutProduct from '../checkout/CheckoutProduct/CheckoutProduct'
 import { Link } from 'react-router-dom'
+import { CardElement,useElements, useStripe } from '@stripe/react-stripe-js'
+
+
+import CurrencyFormat from 'react-currency-format'
+import { subBasketTotal } from '../../reducer' 
+
 function Payment() {
     const[{basket,user}]=useStateValue()
+    const stripe =useStripe()
+    const element=useElements()
+    const [Error,setError]=useState(null)
+    const [disabled,setDisabled]=useState(true)
+
+    const handleSubmit=e=>{
+        
+    }
+    const handleChange=e=>{
+        setDisabled(e.empty)
+        setError(e.error?e.error.message :"")
+    }
+
   return (
     <>
         <Header/>
@@ -48,7 +67,28 @@ function Payment() {
                         <h3>Payment Method</h3>
                 </div>
                     <div className='payment_details'>
+                        <form onSubmit={handleSubmit}>
+                            <CardElement onChange={handleChange}/>
+                            <div className='payment_priceContainer'>
+                            <CurrencyFormat 
 
+                                    renderText={(value)=>{
+                                    return ((
+                                        <>  
+                                        <h3>Order Total : {value}</h3>
+                                        </>
+                                    ))
+                                    }}
+
+                                    decimalScale={2}
+                                    value={subBasketTotal(basket)}
+                                    displayType={'text'}
+                                    thousandSeparator={true}
+                                    prefix={'$'}
+                                    />
+
+                            </div>
+                        </form>
                     </div>
                 </div>
 
