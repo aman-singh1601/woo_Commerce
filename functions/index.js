4d7e4fcf8149e57870d2a103b81806f2d24080e2
Aman <aman16singh2001@gmail.com>
2023-06-29 13:07:51 +0530

    working on the backend

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,24 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const functions=require('firebase-functions')
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const express=require('express')
+const cors=require('cors')
+
+const stripe = require('stripe')('sk_test_51NNBNSSC9WSgNAb5LGEqhVS9iEtiYgG5FU4yKXsspaTS4AJ3mJ1VUiBTsSF5NR2yOXdOLwBpimi5ZFJ1kQcWsW5J00Vaq5jmo5')
+
+//app config
+const app=express()
+//middlewares
+app.use(cors({origin:true}))
+app.use(express.json());
+
+//api routes
+app.get('/',(req,res)=>{
+    res.status(200).send('helloworld')
+})
+
+//listen command
+exports.api=functions.https.onRequest(app)
+
+// http://127.0.0.1:5001/woo-commerce-941c8/us-central1/api
