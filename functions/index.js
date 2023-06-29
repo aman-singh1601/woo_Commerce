ddd96dcb4714ddc1e28017d3ced7e4037f0334ea
Aman <aman16singh2001@gmail.com>
2023-06-29 16:22:08 +0530

    deployed the functions folder

--- functions/index.js
+++ functions/index.js
@@ -1,38 +1,24 @@
+/* eslint-disable */
+const functions=require("firebase-functions");
 
+const express=require("express");
+const cors=require("cors");
+const stripe = require("stripe")
+("sk_test_51NNBNSSC9WSgNAb5LGEqhVS9iEtiYgG5FU4yKXsspaTS4AJ3mJ1VUiBTsSF5NR2yOXdOLwBpimi5ZFJ1kQcWsW5J00Vaq5jmo5");
+const app=express();
 
-const functions=require('firebase-functions')
-
-const express=require('express')
-const cors=require('cors')
-
-const stripe = require('stripe')('sk_test_51NNBNSSC9WSgNAb5LGEqhVS9iEtiYgG5FU4yKXsspaTS4AJ3mJ1VUiBTsSF5NR2yOXdOLwBpimi5ZFJ1kQcWsW5J00Vaq5jmo5')
-
-//app config
-const app=express()
-//middlewares
-app.use(cors({origin:true}))
+app.use(cors({origin: true}));
 app.use(express.json());
-
-//api routes
-app.get('/',(req,res)=>{
-    res.status(200).send('helloworld')
-})
-
-app.post('/payments/create',async(req,res)=>{
+app.post("/payments/create", async(req,res)=>{
     const total=req.query.total;
-
-    console.log('payment for order is ::  ',total)
-
     const paymentIntent=await stripe.paymentIntents.create({
        amount : total,
-       currency: 'INR'
-    })
+       currency: 'INR',
+    });
     res.status(201).send({
-        clientSecret:paymentIntent.client_secret
-    })
-})
+        clientSecret:paymentIntent.client_secret,
+    });
+});
 
-//listen command
-exports.api=functions.https.onRequest(app)
+exports.api=functions.https.onRequest(app);
 
-// http://127.0.0.1:5001/woo-commerce-941c8/us-central1/api
